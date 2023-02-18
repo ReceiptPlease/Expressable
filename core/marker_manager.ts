48e519d6ba1a2af6acc774c22cd40a4ca9e98908
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2023-02-17 19:23:24 -0500

    Refactored marker manager

--- core/marker_manager.ts
+++ core/marker_manager.ts
@@ -1,186 +1,227 @@
-/**
- * @license
- * Copyright 2019 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
 
 /**
- * Object in charge of managing markers and the cursor.
- *
- * @class
+ *  Manages markers and the cursor.
  */
-import * as goog from '../closure/goog/goog.js';
-goog.declareModuleId('Blockly.MarkerManager');
 
-import type {Cursor} from './keyboard_nav/cursor.js';
-import type {Marker} from './keyboard_nav/marker.js';
-import type {WorkspaceSvg} from './workspace_svg.js';
+import type { WorkspaceSvg } from './workspace_svg.js'
+import type { Cursor } from './keyboard_nav/cursor.js'
+import type { Marker } from './keyboard_nav/marker.js'
 
 
-/**
- * Class to manage the multiple markers and the cursor on a workspace.
- */
 export class MarkerManager {
-  /** The name of the local marker. */
-  static readonly LOCAL_MARKER = 'local_marker_1';
 
-  /** The cursor. */
-  private cursor_: Cursor|null = null;
+    /** The name of the local marker. */
 
-  /** The cursor's SVG element. */
-  private cursorSvg_: SVGElement|null = null;
+    static readonly LOCAL_MARKER = 'local_marker_1'
 
-  /** The map of markers for the workspace. */
-  private markers = new Map<string, Marker>();
+    private cursor_: Cursor|null = null
 
-  /** The marker's SVG element. */
-  private markerSvg_: SVGElement|null = null;
+    private markerSvg_ : SVGElement | null = null
+    private cursorSvg_ : SVGElement | null = null
 
-  /**
-   * @param workspace The workspace for the marker manager.
-   * @internal
-   */
-  constructor(private readonly workspace: WorkspaceSvg) {}
+    private markers = new Map<string, Marker>
 
-  /**
-   * Register the marker by adding it to the map of markers.
-   *
-   * @param id A unique identifier for the marker.
-   * @param marker The marker to register.
-   */
-  registerMarker(id: string, marker: Marker) {
-    if (this.markers.has(id)) {
-      this.unregisterMarker(id);
-    }
-    marker.setDrawer(
-        this.workspace.getRenderer().makeMarkerDrawer(this.workspace, marker));
-    this.setMarkerSvg(marker.getDrawer().createDom());
-    this.markers.set(id, marker);
-  }
 
-  /**
-   * Unregister the marker by removing it from the map of markers.
-   *
-   * @param id The ID of the marker to unregister.
-   */
-  unregisterMarker(id: string) {
-    const marker = this.markers.get(id);
-    if (marker) {
-      marker.dispose();
-      this.markers.delete(id);
-    } else {
-      throw Error(
-          'Marker with ID ' + id + ' does not exist. ' +
-          'Can only unregister markers that exist.');
-    }
-  }
+    constructor ( private readonly workspace : WorkspaceSvg ) {}
 
-  /**
-   * Get the cursor for the workspace.
-   *
-   * @returns The cursor for this workspace.
-   */
-  getCursor(): Cursor|null {
-    return this.cursor_;
-  }
 
-  /**
-   * Get a single marker that corresponds to the given ID.
-   *
-   * @param id A unique identifier for the marker.
-   * @returns The marker that corresponds to the given ID, or null if none
-   *     exists.
-   */
-  getMarker(id: string): Marker|null {
-    return this.markers.get(id) || null;
-  }
+    /**
+     * Register the marker by adding it to the map of markers.
+     *
+     * @param id A unique identifier for the marker.
+     * @param marker The marker to register.
+     */
 
-  /**
-   * Sets the cursor and initializes the drawer for use with keyboard
-   * navigation.
-   *
-   * @param cursor The cursor used to move around this workspace.
-   */
-  setCursor(cursor: Cursor) {
-    if (this.cursor_ && this.cursor_.getDrawer()) {
-      this.cursor_.getDrawer().dispose();
-    }
-    this.cursor_ = cursor;
-    if (this.cursor_) {
-      const drawer = this.workspace.getRenderer().makeMarkerDrawer(
-          this.workspace, this.cursor_);
-      this.cursor_.setDrawer(drawer);
-      this.setCursorSvg(this.cursor_.getDrawer().createDom());
-    }
-  }
+    registerMarker ( id : string , marker : Marker ){
 
-  /**
-   * Add the cursor SVG to this workspace SVG group.
-   *
-   * @param cursorSvg The SVG root of the cursor to be added to the workspace
-   *     SVG group.
-   * @internal
-   */
-  setCursorSvg(cursorSvg: SVGElement|null) {
-    if (!cursorSvg) {
-      this.cursorSvg_ = null;
-      return;
+        const { markers } = this;
+
+        if( markers.has(id) )
+            this.unregisterMarker(id);
+
+
+        const { workspace } = this;
+
+        const drawer = workspace
+            .getRenderer()
+            .makeMarkerDrawer(workspace,marker);
+
+        marker.setDrawer(drawer);
+
+
+        const dom = marker
+            .getDrawer()
+            .createDom();
+
+        this.setMarkerSvg(dom);
+
+
+        markers.set(id,marker);
     }
 
-    this.workspace.getBlockCanvas()!.appendChild(cursorSvg);
-    this.cursorSvg_ = cursorSvg;
-  }
 
-  /**
-   * Add the marker SVG to this workspaces SVG group.
-   *
-   * @param markerSvg The SVG root of the marker to be added to the workspace
-   *     SVG group.
-   * @internal
-   */
-  setMarkerSvg(markerSvg: SVGElement|null) {
-    if (!markerSvg) {
-      this.markerSvg_ = null;
-      return;
+    /**
+     * Unregister the marker by removing it from the map of markers.
+     *
+     * @param id The ID of the marker to unregister.
+     */
+
+    unregisterMarker(id: string) {
+
+        const { markers } = this;
+
+        const marker =
+            markers.get(id);
+
+        if( ! marker )
+            throw Error(
+                `Marker with ID ${ id } does not exist.
+                 Can only unregister markers that exist.`);
+
+        marker.dispose();
+        this.markers.delete(id);
     }
 
-    if (this.workspace.getBlockCanvas()) {
-      if (this.cursorSvg_) {
-        this.workspace.getBlockCanvas()!.insertBefore(
-            markerSvg, this.cursorSvg_);
-      } else {
-        this.workspace.getBlockCanvas()!.appendChild(markerSvg);
-      }
-    }
-  }
 
-  /**
-   * Redraw the attached cursor SVG if needed.
-   *
-   * @internal
-   */
-  updateMarkers() {
-    if (this.workspace.keyboardAccessibilityMode && this.cursorSvg_) {
-      this.workspace.getCursor()!.draw();
-    }
-  }
+    /**
+     * Get the cursor for the workspace.
+     *
+     * @returns The cursor for this workspace.
+     */
 
-  /**
-   * Dispose of the marker manager.
-   * Go through and delete all markers associated with this marker manager.
-   *
-   * @suppress {checkTypes}
-   * @internal
-   */
-  dispose() {
-    const markerIds = Object.keys(this.markers);
-    for (let i = 0, markerId; markerId = markerIds[i]; i++) {
-      this.unregisterMarker(markerId);
+    getCursor (){
+        return this.cursor_
     }
-    this.markers.clear();
-    if (this.cursor_) {
-      this.cursor_.dispose();
-      this.cursor_ = null;
+
+
+    /**
+     * Get a single marker that corresponds to the given ID.
+     *
+     * @param id A unique identifier for the marker.
+     * @returns The marker that corresponds to the given ID, or null if none
+     *     exists.
+     */
+
+    getMarker ( id : string ){
+        return this.markers
+            .get(id) ?? null
     }
-  }
+
+
+    /**
+     * Sets the cursor and initializes the drawer for use with keyboard
+     * navigation.
+     *
+     * @param cursor The cursor used to move around this workspace.
+     */
+
+    setCursor ( cursor : Cursor ){
+
+        if( this.cursor_ && this.cursor_.getDrawer() )
+            this.cursor_.getDrawer().dispose();
+
+        this.cursor_ = cursor;
+
+        if( ! this.cursor_ )
+            return
+
+        const { workspace } = this;
+
+        const drawer = workspace
+            .getRenderer()
+            .makeMarkerDrawer(workspace,this.cursor_);
+
+        this.cursor_.setDrawer(drawer);
+
+        this.setCursorSvg(this.cursor_.getDrawer().createDom());
+    }
+
+
+    /**
+     * Add the cursor SVG to this workspace SVG group.
+     *
+     * @param cursor The SVG root of the cursor to be added to the workspace
+     *     SVG group.
+     * @internal
+     */
+
+    setCursorSvg ( cursor : SVGElement | null ){
+
+        if( cursor )
+            this.workspace
+            .getBlockCanvas()
+            ?.appendChild(cursor);
+
+        this.cursorSvg_ = cursor;
+    }
+
+
+    /**
+     * Add the marker SVG to this workspaces SVG group.
+     *
+     * @param marker The SVG root of the marker to be added to the workspace
+     *     SVG group.
+     * @internal
+     */
+
+    setMarkerSvg ( marker : SVGElement | null ){
+
+        if( ! marker ){
+            this.markerSvg_ = null;
+            return
+        }
+
+        const canvas = this.workspace
+            .getBlockCanvas();
+
+        if( ! canvas )
+            return
+
+        if( this.cursorSvg_ )
+            canvas.insertBefore(marker,this.cursorSvg_);
+        else
+            canvas.appendChild(marker);
+    }
+
+    /**
+     * Redraw the attached cursor SVG if needed.
+     *
+     * @internal
+     */
+
+    updateMarkers (){
+
+        const { workspace } = this;
+
+        if( ! workspace.keyboardAccessibilityMode )
+            return
+
+        if( ! this.cursorSvg_ )
+            return
+
+        workspace
+        .getCursor()
+        ?.draw()
+    }
+
+    /**
+     * Dispose of the marker manager.
+     * Go through and delete all markers associated with this marker manager.
+     *
+     * @internal
+     */
+
+    dispose (){
+
+        for ( const marker of this.markers.keys() )
+            this.unregisterMarker(marker);
+
+        this.markers.clear();
+
+        if( ! this.cursor_ )
+            return
+
+        this.cursor_.dispose();
+        this.cursor_ = null;
+    }
 }
